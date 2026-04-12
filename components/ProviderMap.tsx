01a08a10b0a954fd8ba432229322b3c85b65a5f5
Jason <your-email@example.com>
2026-04-12 09:38:18 -0400

    fix: install react-leaflet + clean MapContent.tsx

--- components/ProviderMap.tsx
+++ components/ProviderMap.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
-// Dynamic import to avoid SSR issues with Leaflet (window/document)
-const MapWithNoSSR = dynamic(
-  () => import("./MapContent"), // We'll create this below
-  { ssr: false }
-);
+const MapWithNoSSR = dynamic(() => import("./MapContent"), { ssr: false });
 
 export default function ProviderMap({
   markers,
@@ -19,5 +13,11 @@ export default function ProviderMap({
   center?: [number, number];
   highlightedMarkerId?: string;
 }) {
-  return <MapWithNoSSR markers={markers} center={center} highlightedMarkerId={highlightedMarkerId} />;
+  return (
+    <MapWithNoSSR 
+      markers={markers} 
+      center={center} 
+      highlightedMarkerId={highlightedMarkerId} 
+    />
+  );
 }
